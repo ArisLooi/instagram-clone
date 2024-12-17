7e64a40219c73098e6c5c840dbb81333142bafe9
Aris Looi <arislooi@outlook.com>
2024-12-17 19:05:15 +0800

    add postSlice.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Col, Container, Row } from "react-bootstrap";
 import IconButton from "./components/IconButton";
 import ProfileHeader from "./components/ProfileHeader";
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { PROFILE_DATA } from "./data";
 import ImageGrid from "./components/ImageGrid";
 import Bookmark from "./components/Bookmark";
+import AddPostModal from "./components/AddPostModal";
 
 export const ProfileContext = createContext(null);
 
 export default function App() {
   const profileImageUrl = "./public/me.jpg";
+  const [showModal, setShowModal] = useState(false);
+  const closeModal = () => setShowModal(false);
+  const openModal = () => setShowModal(true);
   return (
     <ProfileContext.Provider value={PROFILE_DATA}>
 
@@ -26,7 +30,7 @@ export default function App() {
           <IconButton className="bi bi-film" />
           <IconButton className="bi bi-chat" />
           <IconButton imageUrl={profileImageUrl} />
-          <IconButton className="bi bi-plus-square" />
+          <IconButton className="bi bi-plus-square" onClick={openModal} />
           <IconButton className="bi bi-list" isBottom />
         </Col>
         <Col sm={11}>
@@ -34,6 +38,7 @@ export default function App() {
             <ProfileHeader />
             <Bookmark />
             <ImageGrid />
+            <AddPostModal show={showModal} handleClose={closeModal} />
           </Container>
         </Col>
       </Row >
