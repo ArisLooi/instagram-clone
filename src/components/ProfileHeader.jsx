54d209bd10c1c40995dea42be2bf29cde92c3e8d
Aris Looi <arislooi@outlook.com>
2024-12-17 10:45:03 +0800

    update ProfileHeader.jsx

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,9 +1,15 @@
-import { Col, Row } from "react-bootstrap";
+import { Col, Image, Row } from "react-bootstrap";
 
 export default function ProfileHeader() {
     return (
-        <Row>
-            <Col md={3}>Profile Image</Col>
+        <Row className="p-5">
+            <Col md={3} className="d-flex align-items-center justify-content-center">
+                <Image
+                    src={"https://sig1.co/logo-1"}
+                    style={{ height: "150px" }}
+                    roundedCircle
+                />
+            </Col>
             <Col md={3}>Profile Description</Col>
         </Row>
     )
